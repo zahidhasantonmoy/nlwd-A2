1b1054f30d4a00947f2ad3fe68c16c13ae5e3663
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2026-09-17 22:18:00 +0600

    route done

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,17 @@ import Header from './Component/Header'
 import Home from './Pages/Home'
 import Movies from './Pages/Movies'
 
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  
-
   return (
-    <>
-     <Header/>
-      <Home/>
-      <Movies/>
-   
-    </>
-  )
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/Movies" element={<Movies/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
 export default App
